33a408e69e7be28d4d4aa00639253f0160f4f3cf
fmachicao <fabian.machicao@dharbor.com>
2025-03-10 02:08:43 -0400

    Finish merging

---
+++ src/app/components/dt-full-screen/dt-full-screen.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dt-full-screen',
+  templateUrl: './dt-full-screen.component.html',
+  styleUrls: ['./dt-full-screen.component.scss']
+})
+export class DtFullScreenComponent   {
+
+  constructor() { }
+
+}
